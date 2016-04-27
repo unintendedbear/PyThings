b7dcd2545e4420389332e3f998961d26d73d6fd4
unintendedbear <paloma.cuevas.d@gmail.com>
2016-04-27 18:57:42 +0200

    Cambiando el nombre para tener distintos tipos de variables por separado

---
+++ variables_numericas.py
@@ -0,0 +1,39 @@
+#!/user/bin/python
+# -*- coding: utf-8 -*-
+"""variables.py"""
+
+# Tipos de variables que podemos crear y tipos de datos que podemos manejar
+
+# Numéricos
+# Antes de concatenar números, los tenemos que transformar a cadena
+
+enteroA = 4
+enteroAA = int(4) # int() a secas da 0
+enteroB = 6
+enteroBB = int(6)
+enteroC = enteroA + enteroB
+enteroCC = enteroAA + enteroBB
+
+print enteroC, enteroCC
+
+entero_largo = 145L
+long()
+
+print entero_largo
+
+flotante = 1.797
+float()
+
+print flotante
+
+complejo = 34 + 5j
+complex()
+
+print complejo
+
+logico = True, False
+bool()
+
+print logico
+
+
