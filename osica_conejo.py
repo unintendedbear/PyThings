72a7e468d998b599ab984387dd83c1cc1a1c2334
unintendedbear <paloma.cuevas.d@gmail.com>
2016-04-27 19:34:56 +0200

    Mi primera función en Python :DDD

---
+++ osica_conejo.py
@@ -0,0 +1,14 @@
+#!/user/bin/python
+# -*- coding: utf-8 -*-
+"""osica_conejo.py"""
+
+def osico_canibal(caballero): # Las variables pueden ser todas las que se quieran
+
+	if caballero == "Antares":
+		print "A Antares no!"
+	else:
+		print "El osico asesina a Sir "+caballero
+
+osico_canibal("Ángel")
+osico_canibal("Fergu")
+osico_canibal("Antares")
