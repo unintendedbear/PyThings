8eccae4a8e43551c81823749233b6bf6fbf5f3b4
unintendedbear <paloma.cuevas.d@gmail.com>
2016-04-27 19:16:41 +0200

    Condicionales y gansos

---
+++ osica_bruja.py
@@ -0,0 +1,17 @@
+#!/user/bin/python
+# -*- coding: utf-8 -*-
+"""osica_bruja.py"""
+
+# Condicionales
+
+peso = 50
+peso_de_un_ganso = 5
+
+if peso < peso_de_un_ganso:
+	print "¡Es una bruja!" # NO HAY llaves, la indentación implica que está dentro de la función
+
+elif peso > peso_de_un_ganso: # No "elsif"
+	print "Ah, pues no lo es"
+
+else:
+	print "Pues a mí me convirtió en grillo... y mejoré"
