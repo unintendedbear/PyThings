33070d6c570bdb2c0a121cb8e92a0f3049a05185
unintendedbear <paloma.cuevas.d@gmail.com>
2016-04-27 19:31:11 +0200

    Manejando excepciones y tipos de error

--- excepciones.py
+++ excepciones.py
@@ -18,4 +18,18 @@ except:
 
 # De otra manera:
 
+dividendo2 = "A"
+divisor2 = 2
 
+try:
+	resultado2 = dividendo2/divisor2
+
+except ZeroDivisionError:
+	if divisor == 0:
+		print "No se puede hacer"
+
+except TypeError:
+	print "Eso no es un numero"
+
+else: # Se ejecuta si no ha habido error en el try
+	print "Resultado2: "+str(resultado2)
